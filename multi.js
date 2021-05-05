3f56e959cb4ab554a60dc860ac40b9eca19f5727
Ven <0ventox0@gmail.com>
2021-05-05 20:56:22 +0200

    added workers

--- multi.js
+++ multi.js
@@ -1,4 +1,4 @@
-const {Worker} = require("worker_threads");
+const { Worker } = require("worker_threads");
 
 const d = [];
 
@@ -34,85 +34,82 @@ const d = [];
 let result = 0;
 
 
-const num =Number(process.argv[2]);
-if(process.argv[3] === undefined)
-{
+const num = Number(process.argv[2]);
+if (process.argv[3] === undefined) {
     process.argv[3] = 10;
     console.warn("Keine 2. Zahl agegeben, nehme 10 Runden Standard")
 }
 (async function() {
-    console.log(num);
-        for(let i = 0; i < process.argv[3]; i++)
-        {
-            const workers = [];
-            switch(true)
-            {
-                case (num >0): 
-                case (num <16):
-                    for(let o = 0; o < num; o++)
-                    {
-                        workers.push(new Worker(`./workers/worker${num}.js`));
-                    } break;
-                default: throw("Ungültige Nummer");
-            }
-
-            //const worker1 =  new Worker('./workers/worker1.js');
-            // const worker2 =  new Worker('./workers/worker2.js');
-            // const worker3 =  new Worker('./workers/worker3.js');
-            // const worker4 =  new Worker('./workers/worker4.js');
-            // const worker5 =  new Worker('./workers/worker5.js');
-            // const worker6 =  new Worker('./workers/worker6.js');
-            // const worker7 =  new Worker('./workers/worker7.js');
-            // const worker8 =  new Worker('./workers/worker8.js');
-            // const worker9 =  new Worker('./workers/worker9.js');
-            // const worker10 = new Worker('./workers/worker10.js');
-            // const worker11 = new Worker('./workers/worker11.js');
-            // const worker12 = new Worker('./workers/worker12.js');
-            // const worker13 = new Worker('./workers/worker13.js');
-            // const worker14 = new Worker('./workers/worker14.js');
-            // const worker15 = new Worker('./workers/worker15.js');
-            // const worker16 = new Worker('./workers/worker16.js');
-            //console.dir(workers[0]);
-            let P = [
-                //new Promise(resolve => {workers[].on("exit", () => {resolve();})})
-            ];
-            workers.forEach((w,i) => {
-                P.push(new Promise(resolve => {w.on("exit", () => resolve())}))
-            })
-            const t0 = performance.now();
-
-            workers.forEach((w, i) => {
-                w.postMessage([num, i+1]); //w präsentiert das Objekt. console.log => w.log(i);
-            });
-            // worker1.postMessage(true);
-            // worker2.postMessage(true);
-            // worker3.postMessage(true);
-            // worker4.postMessage(true);
-            // worker5.postMessage(true);
-            // worker6.postMessage(true);
-            // worker7.postMessage(true);
-            // worker8.postMessage(true);
-            // worker9.postMessage(true);
-            // worker10.postMessage(true);
-            // worker11.postMessage(true);
-            // worker12.postMessage(true);
-            // worker13.postMessage(true);
-            // worker14.postMessage(true);
-            // worker15.postMessage(true);
-            // worker16.postMessage(true);
-            //console.dir(P);
-            await Promise.all(P).then(() => {
-                const t1 = performance.now();
-                console.log(`Main function took ${t1-t0}ms`);
-                d.push(t1-t0);
-            }).catch(e => {throw e;});
-            
+    //console.log(num);
+    for (let i = 0; i < process.argv[3]; i++) {
+        const workers = [];
+        switch (true) {
+            case (num > 0):
+            case (num < 16):
+                for (let o = 0; o < num; o++) {
+                    workers.push(new Worker(`./workers/worker${num}.js`));
+                }
+                break;
+            default:
+                throw ("Ungültige Nummer");
         }
-        d.forEach(i => {
-            result+=i;
-        });
-        console.log(result);
-        console.log(result/process.argv[3]);
-        console.log((result/process.argv[3])/process.argv[2]);
-})();
 
+        //const worker1 =  new Worker('./workers/worker1.js');
+        // const worker2 =  new Worker('./workers/worker2.js');
+        // const worker3 =  new Worker('./workers/worker3.js');
+        // const worker4 =  new Worker('./workers/worker4.js');
+        // const worker5 =  new Worker('./workers/worker5.js');
+        // const worker6 =  new Worker('./workers/worker6.js');
+        // const worker7 =  new Worker('./workers/worker7.js');
+        // const worker8 =  new Worker('./workers/worker8.js');
+        // const worker9 =  new Worker('./workers/worker9.js');
+        // const worker10 = new Worker('./workers/worker10.js');
+        // const worker11 = new Worker('./workers/worker11.js');
+        // const worker12 = new Worker('./workers/worker12.js');
+        // const worker13 = new Worker('./workers/worker13.js');
+        // const worker14 = new Worker('./workers/worker14.js');
+        // const worker15 = new Worker('./workers/worker15.js');
+        // const worker16 = new Worker('./workers/worker16.js');
+        //console.dir(workers[0]);
+        let P = [
+            //new Promise(resolve => {workers[].on("exit", () => {resolve();})})
+        ];
+        workers.forEach((w, i) => {
+            P.push(new Promise(resolve => { w.on("exit", () => resolve()) }))
+        })
+        const t0 = performance.now();
+
+        workers.forEach((w, i) => {
+            w.postMessage([num, i + 1]); //w präsentiert das Objekt. console.log => w.log(i);
+        });
+        // worker1.postMessage(true);
+        // worker2.postMessage(true);
+        // worker3.postMessage(true);
+        // worker4.postMessage(true);
+        // worker5.postMessage(true);
+        // worker6.postMessage(true);
+        // worker7.postMessage(true);
+        // worker8.postMessage(true);
+        // worker9.postMessage(true);
+        // worker10.postMessage(true);
+        // worker11.postMessage(true);
+        // worker12.postMessage(true);
+        // worker13.postMessage(true);
+        // worker14.postMessage(true);
+        // worker15.postMessage(true);
+        // worker16.postMessage(true);
+        //console.dir(P);
+        await Promise.all(P).then(() => {
+            const t1 = performance.now();
+            console.log(`Main function took ${Math.round(t1-t0)}ms`);
+            d.push(t1 - t0);
+        }).catch(e => { throw e; });
+
+    }
+    d.forEach(i => {
+        result += i;
+    });
+    console.log(`Time took for all tasks: ${Math.round(result)}ms`);
+    console.log(`Time took per task(avg): ${Math.round(result / process.argv[3])}ms`);
+    console.log(`Time took per task per thread (avg): ${Math.round((result / process.argv[3]) / process.argv[2])}ms`);
+})();
--- single.js
+++ single.js
@@ -1,37 +1,17 @@
-const {Worker} = require("worker_threads");
+const { Worker } = require("worker_threads");
 const bcrypt = require('bcrypt');
 const d = [];
 
+// const argv = require("yargs/yargs")(process.argv.slice(2)).usage('Usage: -threads [num] -tasks [num]').demand(['threads', 'tasks']).argv;
 
-// worker1.on('message', e => {
-//     console.log(e);
-// });
-// worker2.on('message', e => {
-//     console.log(e);
-// });
-// worker3.on('message', e => {
-//     console.log(e);
-// });
+// console.log(argv.threads, argv.tasks);
+// console.dir(argv);
 
-// worker1.on("exit", e => {
-//     console.log("owo");
-//     P1.resolve();
-// });
+const settings = {
 
-// worker2.on("exit", e => {
-//     P2.resolve();
-// })
+}
 
-
-// worker3.on("exit", e => {
-//     P3.resolve();
-// })
-
-// let P1 = new Promise(resolve => {worker1.on("exit", () => {resolve();})});
-// let P2 = new Promise(resolve => {worker2.on("exit", () => {resolve();})});
-// let P3 = new Promise(resolve => {worker3.on("exit", () => {resolve();})});
-const x = () =>
-{
+const x = () => {
     return new Promise(resolve => {
         const t0 = performance.now();
         for (let i = 0; i < 1000; i++) {
@@ -39,35 +19,34 @@ const x = () =>
             bcrypt.hashSync(Math.random().toString(), 10);
         }
         const t1 = performance.now();
-        resolve(`Hashing Math.random() 333 times with 10 salts took ${t1-t0}ms`);
+        resolve(`Hashing Math.random() 1000 times with 10 salts took ${t1-t0}ms`);
     });
 }
 
 let result = 0;
 
 (async function() {
-        for(let i = 0; i < 10; i++)
-        {
-            const worker1 = new Worker('./worker1.js');
-            const worker2 = new Worker('./worker2.js');
-            const worker3 = new Worker('./worker3.js');
-            let P = [
-                new Promise(async resolve => {await x().catch(e => {throw e;}).then(() => {resolve();})})
-            ];
-            const t0 = performance.now();
-            // worker1.postMessage(true);
-            // worker2.postMessage(true);
-            // worker3.postMessage(true);
-            await x().catch(e => {throw e}).then(() => {
-                const t1 = performance.now();
-                console.log(`Main function took ${t1-t0}ms`);
-                d.push(t1-t0);
-            });
-            
-        }
-        d.forEach(i => {
-            result+=i;
+    for (let i = 0; i < 10; i++) {
+        const worker1 = new Worker('./worker1.js');
+        const worker2 = new Worker('./worker2.js');
+        const worker3 = new Worker('./worker3.js');
+        let P = [
+            new Promise(async resolve => { await x().catch(e => { throw e; }).then(() => { resolve(); }) })
+        ];
+        const t0 = performance.now();
+        // worker1.postMessage(true);
+        // worker2.postMessage(true);
+        // worker3.postMessage(true);
+        await x().catch(e => { throw e }).then(() => {
+            const t1 = performance.now();
+            console.log(`Main function took ${Math.round(t1-t0)}ms`);
+            d.push(t1 - t0);
         });
-        console.log(result);
-        console.log(result/10);
+
+    }
+    d.forEach(i => {
+        result += i;
+    });
+    console.log(`Time took for all tasks:${Math.round(result)}`);
+    console.log(`Time took per task(avg): ${Math.round(result/process.argv[2])}`);
 })();
